30b6a5c2ca28ba74aa0eb3775dee46eabae570d6
r3-endo <endo.ryo.mm@gmail.com>
2023-04-01 13:39:42 +0900

    chart.jsの導入

--- cost-manager-frontend/src/pages/Household/Home.tsx
+++ cost-manager-frontend/src/pages/Household/Home.tsx
@@ -1,25 +1,54 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import Header from '../../components/Header'
-import { Pie } from 'react-chartjs-2'
-const labels = ['January', 'February', 'March', 'April', 'May', 'June']
-const data = {
-  labels: labels,
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  DoughnutController,
+} from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  DoughnutController
+)
+
+const chartData = {
+  labels: ['Red', 'Blue', 'Yellow'],
   datasets: [
     {
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(0,0,255)',
-      data: [0, 10, 5, 2, 20, 30, 45],
+      label: '# of Votes',
+      data: [12, 19, 3],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      borderWidth: 1,
     },
   ],
 }
+
+const chartOptions = {
+  responsive: true,
+}
+
 const Home: React.FC = () => {
   return (
     <div>
       <Header />
       <div>
-        <h2>総資産</h2>
-        <Pie data={data} />
+        <h2>総資産推移</h2>
+        <Bar data={chartData} options={chartOptions} />
       </div>
     </div>
   )
